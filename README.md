2d2f54e9d8dd0d706d65051ec0b8b209380dbef9
Michael Banzon <mrnbanzon@gmail.com>
2019-07-26 14:44:12 -0700

    Test strings against regex.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # practice-with-jest
+
 This repository is used for practicing testing with Jest.
+
+It follows the examples presented in the jest documentation at https://jestjs.io
---
+++ tests/strings.test.js
@@ -0,0 +1,8 @@
+// test strings against regular expressions with toMatch
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/);
+});
